0433224934b477e6bdaa66655742f7eb4eb25081
JuanRomeroRuiz <167258758+JuanRomeroRuiz@users.noreply.github.com>
2025-06-17 08:11:34 -0600

    Mostrar resultados y validar ganador

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -15,18 +15,25 @@ function hasPlayerWonTheRound(player, computer) {
   );
 }
 
-function getRoundResults(userOption) {
-  const computerResult = getRandomComputerResult();
- 
-  if (userOption === computerResult) {
-    return `It's a tie! Both chose ${userOption}`;
-  }
+const playerScoreSpanElement = document.getElementById("player-score");
+const computerScoreSpanElement = document.getElementById("computer-score");
+const roundResultsMsg = document.getElementById("results-msg");
+const winnerMsgElement = document.getElementById("winner-msg");
+const optionsContainer = document.querySelector(".options-container");
+const resetGameBtn = document.getElementById("reset-game-btn");
 
-  if (hasPlayerWonTheRound(userOption, computerResult)) {
-    playerScore += 1;
-    return `Player wins! ${userOption} beats ${computerResult}`;
-  } else {
-    computerScore += 1;
-    return `Computer wins! ${computerResult} beats ${userOption}`;
+function showResults(userOption) {
+  roundResultsMsg.innerText = getRoundResults(userOption);
+  computerScoreSpanElement.innerText = computerScore;
+  playerScoreSpanElement.innerText = playerScore;
+
+  if (playerScore === 3) {
+    winnerMsgElement.innerText = "Player has won the game!";
+    resetGameBtn.style.display = "block";
+    optionsContainer.style.display = "none";
+  } else if (computerScore === 3) {
+    winnerMsgElement.innerText = "Computer has won the game!";
+    resetGameBtn.style.display = "block";
+    optionsContainer.style.display = "none";
   }
-}
+};
